1bac412c60d96aef9a9d279e045ca2162ca0c978
ethles <percivalstr@163.com>
2019-07-02 20:26:57 +0800

    implement pseudo-encrypt algorithm

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	unqiue "github.com/ethles/unqiue-name/unique"
+)
+
+func main() {
+	fmt.Println(unqiue.SimplePseudoEncrypt(2))
+}
---
+++ unique/unique.go
@@ -0,0 +1,18 @@
+package unique
+
+func SimplePseudoEncrypt(val int) int {
+	var l1 int = (val >> 16) & 65535
+	var r1 int = val & 65535
+	var l2 int
+	var r2 int
+	var i int = 0
+
+	for i < 3 {
+		l2 = r1
+		r2 = l1 ^ int((float64((1366*r1+150889)%714025)/714025.0)*32767)
+		l1 = l2
+		r1 = r2
+		i = i + 1
+	}
+	return ((r1 << 16) + l1)
+}
